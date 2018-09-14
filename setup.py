6c0de6589d0282d42c402161fe62a71f9d855844
Xavier Barbosa <clint.northwood@gmail.com>
2018-09-14 11:36:09 +0200

    release to pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, "README.rst")).read()
 NEWS = open(os.path.join(here, "NEWS.txt")).read()
 
 
-version = "0.6.0a1"
+version = "0.6.0"
 
 install_requires = ["pylint"]
 
