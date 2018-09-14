5f49a5829d20611a3f1bf808ddf0e41a55d09056
Xavier Barbosa <clint.northwood@gmail.com>
2018-09-14 11:32:49 +0200

    bumps version

--- setup.py
+++ setup.py
@@ -2,49 +2,44 @@ from setuptools import setup, find_packages
 import os
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
-NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+README = open(os.path.join(here, "README.rst")).read()
+NEWS = open(os.path.join(here, "NEWS.txt")).read()
 
 
-version = '0.5.2'
+version = "0.6.0a1"
 
-install_requires = [
-    'pylint'
-]
+install_requires = ["pylint"]
 
 
-setup(name='setuptools-lint',
-      version=version,
-      description="Setuptools command for pylint",
-      long_description=README + '\n\n' + NEWS,
-      classifiers=[
-          "Topic :: Documentation",
-          "Framework :: Setuptools Plugin",
-          "Development Status :: 4 - Beta",
-          "Intended Audience :: Developers",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 3",
-          'License :: OSI Approved :: BSD License',
-          "Topic :: Software Development :: Documentation",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
-      keywords='pylint setuptools command',
-      author='Xavier Barbosa',
-      author_email='',
-      url='https://github.com/johnnoone/setuptools-pylint',
-      license='BSD',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=install_requires,
-      entry_points={
-          "distutils.commands": [
-              "lint = setuptools_lint.setuptools_command:PylintCommand",
-          ],
-          "distutils.setup_keywords": [
-              "lint_rcfile = setuptools_lint.setuptools_command:validate_rcfile",  # NOQA
-          ],
-      }
+setup(
+    name="setuptools-lint",
+    version=version,
+    description="Setuptools command for pylint",
+    long_description=README + "\n\n" + NEWS,
+    classifiers=[
+        "Topic :: Documentation",
+        "Framework :: Setuptools Plugin",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="pylint setuptools command",
+    author="Xavier Barbosa",
+    author_email="",
+    url="https://github.com/johnnoone/setuptools-pylint",
+    license="BSD",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        "distutils.commands": ["lint = setuptools_lint.setuptools_command:PylintCommand"],
+        "distutils.setup_keywords": ["lint_rcfile = setuptools_lint.setuptools_command:validate_rcfile"],  # NOQA
+    },
 )
